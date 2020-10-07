c79eb6aa3f62a8e6a457634166a6d94a6d4c581c
letrananhvu <letrananhvu>
2020-10-07 13:16:08 +0300

    analyst the image is done

--- functions/index.js
+++ functions/index.js
@@ -1,68 +1,34 @@
 const functions = require('firebase-functions');
-const { v4: uuidv4 } = require('uuid');
+const {log} = require('firebase-functions/lib/logger');
 
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-
-
+const vision = require('@google-cloud/vision');
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+const client = new vision.ImageAnnotatorClient();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onFinalize(async (object) => {
+exports.processImage = functions.storage.object().onFinalize(async (object) => {
   const bucket = object.bucket;
   const contentType = object.contentType;
   const filePath = object.name;
-  const fileName = path.basename(filePath);
-  const destBucket = admin.storage().bucket(bucket);
-  const prefix = 'Renamed-';
 
+  // check the file type is image or not
   if (!contentType.startsWith('image/')) {
     return console.log('This is not an image.');
   }
 
+  console.log('Starting image processing . . .');
 
-  if (fileName.startsWith(prefix)) {
-    // Get the Signed URLs for the thumbnail and original image.
-    // const config = {
-    //   action: 'read',
-    //   expires: '03-01-2500',
-    // };
+  // start analyse the image
+  const [result] = await client.labelDetection(
+    `gs://${bucket}/${filePath}`
+  );
 
-    // const renamedFile = destBucket.file(fileName);
-
-    // const responses = await renamedFile.getSignedUrl(config);
-
-
-    return console.log('skip this file');
-  }
-
-  console.log('File change detected, function execution started');
-
-  const tmpFilePath = path.join(os.tmpdir(), filePath);
-
-
-  await destBucket.file(filePath).download({destination: tmpFilePath});
-
-  const uuid = uuidv4();
-  const metadata = {
-    contentType: contentType,
-    metadata: {
-      firebaseStorageDownloadTokens: uuid
-    }
-
-  };
-  await destBucket.upload(tmpFilePath, {
-    destination: prefix + fileName,
-    metadata: metadata,
-    predefinedAcl: 'publicRead'
-  });
-
-  // remove the temporary file
-  fs.unlinkSync(tmpFilePath);
-
-  return console.log('done');
+  // extract the labels fields to review`
+  const labels = result.labelAnnotations;
+  log('Labels:', labels);
+  return;
 });
